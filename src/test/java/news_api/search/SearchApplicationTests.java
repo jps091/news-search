387cf1351de55d78c987ea345086e4c0fa880810
jaepil <jaepill94@gmail.com>
2024-12-31 15:10:34 +0900

    initial commit

---
+++ src/test/java/news_api/search/SearchApplicationTests.java
@@ -0,0 +1,13 @@
+package news_api.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
