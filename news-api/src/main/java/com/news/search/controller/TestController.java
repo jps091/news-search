1a2b3de16b8e87513677286beaea5bd63ab1deeb
jaepil <jaepill94@gmail.com>
2025-01-05 20:10:26 +0900

    [feat/test-19] 비동기 이벤트 처리 도입
    - 이벤트 핸들러 구현
    - 비동기 @async 스레드 풀 설정 구현
    - 테스트 코드 작성
    - Resolves: #19

--- news-api/src/main/java/com/news/search/controller/TestController.java
+++ news-api/src/main/java/com/news/search/controller/TestController.java
@@ -4,6 +4,7 @@ import com.news.search.controller.request.SearchRequest;
 import com.news.search.controller.response.PageResult;
 import com.news.search.controller.response.SearchResponse;
 import com.news.search.controller.response.StatResponse;
+import com.news.search.service.TestService;
 import com.news.search.service.WebApplicationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,7 @@ import java.util.List;
 @RequestMapping("/api/v1/test")
 public class TestController {
     private final WebApplicationService webApplicationService;
+    private final TestService testService;
 
     @GetMapping
     public PageResult<SearchResponse> search(@Valid SearchRequest request){
@@ -27,7 +29,7 @@ public class TestController {
 
     @GetMapping("/jpa")
     public PageResult<SearchResponse> searchByJpa(@Valid SearchRequest request){
-        return webApplicationService.searchByJpa(request.getQuery(), request.getPage(), request.getSize());
+        return testService.searchByJpa(request.getQuery(), request.getPage(), request.getSize());
     }
 
     @GetMapping("/stats")
@@ -43,17 +45,17 @@ public class TestController {
                                        @RequestParam(name = "date", required = false)
                                        LocalDate date) {
         LocalDate localDate = (date != null) ? date : LocalDate.now();
-        return webApplicationService.findQueryCountByJpa(query, localDate);
+        return testService.findQueryCountByJpa(query, localDate);
     }
 
     @PostMapping
     public Long create(@RequestParam(name = "query") String query){
-        return webApplicationService.create(query);
+        return testService.create(query);
     }
 
     @PostMapping("/jpa")
     public Long createByJpa(@RequestParam(name = "query") String query){
-        return webApplicationService.createByJpa(query);
+        return testService.createByJpa(query);
     }
 
     @GetMapping("/stats/ranking")
@@ -63,6 +65,6 @@ public class TestController {
 
     @GetMapping("/jpa/stats/ranking")
     public List<StatResponse> findTop5StatsByJpa() {
-        return webApplicationService.findTop5QueryByJpa();
+        return testService.findTop5QueryByJpa();
     }
 }
--- news-api/src/main/java/com/news/search/service/TestService.java
+++ news-api/src/main/java/com/news/search/service/TestService.java
@@ -1,10 +1,12 @@
 package com.news.search.service;
 
-import com.news.dailystat.service.response.DailyStatQueryResponse;
-import com.news.search.controller.response.StatResponse;
+import com.news.dailystat.model.DailyStat;
+import com.news.dailystat.service.DailyStatCommandService;
 import com.news.dailystat.service.DailyStatQueryService;
+import com.news.dailystat.service.response.DailyStatQueryResponse;
 import com.news.search.controller.response.PageResult;
 import com.news.search.controller.response.SearchResponse;
+import com.news.search.controller.response.StatResponse;
 import com.news.search.service.event.SearchEvent;
 import com.news.search.service.response.PageQueryResult;
 import com.news.search.service.response.SearchQueryResponse;
@@ -20,27 +22,48 @@ import java.util.List;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class WebApplicationService {
+public class TestService {
     private final WebQueryService webQueryService;
+    private final DailyStatCommandService dailyStatCommandService;
     private final DailyStatQueryService dailyStatQueryService;
-    private final ApplicationEventPublisher eventPublisher;
+
+    /**아래는 테스트용 메서드*/
 
     public PageResult<SearchResponse> search(String query, int page, int size){
         PageQueryResult<SearchQueryResponse> pageQueryResponse = webQueryService.search(query, page, size);
-        if(isNotEmptyQueryResponse(pageQueryResponse)){
-            log.info("검색결과 개수: {}", pageQueryResponse.size());
-            eventPublisher.publishEvent(new SearchEvent(query, LocalDateTime.now()));
-        }
-        return convertToPageResult(pageQueryResponse);
+        PageResult<SearchResponse> pageResponse = convertToPageResult(pageQueryResponse);
+        DailyStat dailyStat = DailyStat.create(query, LocalDateTime.now());
+        dailyStatCommandService.save(dailyStat);
+        return pageResponse;
     }
 
-    public StatResponse findQueryCount(String query, LocalDate date) {
-        DailyStatQueryResponse queryResponse = dailyStatQueryService.findQueryCount(query, date);
+    public PageResult<SearchResponse> searchByJpa(String query, int page, int size){
+        PageQueryResult<SearchQueryResponse> pageQueryResponse = webQueryService.search(query, page, size);
+        PageResult<SearchResponse> pageResponse = convertToPageResult(pageQueryResponse);
+        DailyStat dailyStat = DailyStat.create(query, LocalDateTime.now());
+        dailyStatCommandService.saveByJpa(dailyStat);
+        return pageResponse;
+    }
+
+    public Long create(String query){
+        DailyStat dailyStat = DailyStat.create(query, LocalDateTime.now());
+        dailyStatCommandService.save(dailyStat);
+        return dailyStat.getId();
+    }
+
+    public Long createByJpa(String query){
+        DailyStat dailyStat = DailyStat.create(query, LocalDateTime.now());
+        dailyStatCommandService.saveByJpa(dailyStat);
+        return dailyStat.getId();
+    }
+
+    public StatResponse findQueryCountByJpa(String query, LocalDate date) {
+        DailyStatQueryResponse queryResponse = dailyStatQueryService.findQueryCountByJpa(query, date);
         return new StatResponse(queryResponse.query(), queryResponse.count());
     }
 
-    public List<StatResponse> findTop5Query() {
-        List<DailyStatQueryResponse> queryResponse = dailyStatQueryService.findTop5Query();
+    public List<StatResponse> findTop5QueryByJpa() {
+        List<DailyStatQueryResponse> queryResponse = dailyStatQueryService.findTop5QueryByJpa();
         return queryResponse.stream()
                 .map(this::toStatResponse)
                 .toList();
@@ -59,9 +82,5 @@ public class WebApplicationService {
 
     private StatResponse toStatResponse(DailyStatQueryResponse queryResponse){
         return new StatResponse(queryResponse.query(), queryResponse.count());
-    }
-
-    private static boolean isNotEmptyQueryResponse(PageQueryResult<SearchQueryResponse> pageQueryResponse) {
-        return !pageQueryResponse.contents().isEmpty();
     }
 }
